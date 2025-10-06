cc9f4f8b63c0a967056a95695ca8be38dcefcfaa
Constantin Blessing <constantin.blessing@hs-esslingen.de>
2025-10-06 18:43:48 +0200

    sfg_utils: Fixed generate_python_stubs.cmake not working with new devcontainer.

--- sfg_utils/cmake/generate_python_stubs.cmake
+++ sfg_utils/cmake/generate_python_stubs.cmake
@@ -18,7 +18,7 @@ function(generate_python_stubs TARGET_NAME)
     set(stub_directory "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}")
 
     add_custom_target("${target_name}" ALL
-        COMMAND "${PYBIND11_STUBGEN_EXECUTABLE}" -o "${CMAKE_CURRENT_BINARY_DIR}" ${ARG_STUBGEN_ARGS} "${TARGET_NAME}"
+        COMMAND "${Python3_EXECUTABLE}" -m pybind11_stubgen -o "${CMAKE_CURRENT_BINARY_DIR}" ${ARG_STUBGEN_ARGS} "${TARGET_NAME}"
         DEPENDS "${TARGET_NAME}"
         WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
         COMMENT "Generating python stubs for ${TARGET_NAME}."
