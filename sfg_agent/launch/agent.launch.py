3c3e5b2338b69df0d8490ff9a82bc0dbf62465b2
Constantin Blessing <constantin.blessing@hs-esslingen.de>
2025-10-17 15:38:58 +0200

    sfg_agent: Removed get_nodes from agent.launch.py.

--- sfg_agent/launch/agent.launch.py
+++ sfg_agent/launch/agent.launch.py
@@ -1,9 +1,7 @@
-from typing import Any
-
 import launch
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import ComposableNodeContainer, Node
+from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 from rospkg import get_package_name
 from sfg_utils.fqn import RosFqnBuilder, RosFqnSegment, Scope
@@ -20,46 +18,38 @@ local_namespace, global_namespace = (
     .build(begin=RosFqnSegment.Scope, end=RosFqnSegment.Agent),
 )
 
-metadata_filepath_argument = DeclareLaunchArgument(
-    "metadata_filepath", default_value=""
-)
 
+def generate_launch_description() -> launch.LaunchDescription:
+    metadata_filepath_argument = DeclareLaunchArgument(
+        "metadata_filepath", default_value=""
+    )
 
-def get_nodes(**arguments: Any) -> tuple[list[Node], list[ComposableNode]]:
     agent_status_provider_node = ComposableNode(
         package=package_name,
         plugin="sfg_agent::AgentStatusProvider",
         namespace=local_namespace,
         parameters=[
             {
-                metadata_filepath_argument.name: arguments.get(
-                    metadata_filepath_argument.name,
-                    metadata_filepath_argument.default_value,
+                metadata_filepath_argument.name: LaunchConfiguration(
+                    metadata_filepath_argument.name
                 )
             },
         ],
         extra_arguments=[{"use_intra_process_comms": True}],
     )
 
-    return [], [agent_status_provider_node]
-
-
-def generate_launch_description() -> launch.LaunchDescription:
-    nodes, composable_nodes = get_nodes(
-        metadata_filepath=LaunchConfiguration(metadata_filepath_argument.name)
-    )
-
     return launch.LaunchDescription(
         [
             metadata_filepath_argument,
-            *nodes,
             ComposableNodeContainer(
                 package="rclcpp_components",
                 executable="component_container_mt",
                 namespace=local_namespace,
                 name="agent_container",
                 output="screen",
-                composable_node_descriptions=composable_nodes,
+                composable_node_descriptions=[
+                    agent_status_provider_node,
+                ],
             ),
         ]
     )
