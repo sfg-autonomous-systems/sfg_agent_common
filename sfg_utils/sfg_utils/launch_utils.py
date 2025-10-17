498f3253a6a20b6e67143b3f64288693095a18bf
Constantin Blessing <constantin.blessing@hs-esslingen.de>
2025-10-17 15:32:01 +0200

    sfg_utils: Updated launch_utils.py to no longer rely on an additional magic get_nodes function.

--- sfg_utils/sfg_utils/launch_utils.py
+++ sfg_utils/sfg_utils/launch_utils.py
@@ -1,18 +1,24 @@
 import importlib.util
 import inspect
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Any
 
 from ament_index_python.packages import get_package_share_directory
-from launch_ros.actions import Node
+from launch import LaunchDescription, LaunchDescriptionEntity
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import ComposableNodeContainer, Node
 from launch_ros.descriptions import ComposableNode
 
 
-def get_nodes(
-    package_name: str,
-    launch_file: str,
-    **arguments: Any,
-) -> tuple[list[Node], list[ComposableNode]]:
+@dataclass(frozen=True)
+class LaunchDescriptionEntities:
+    nodes: list[Node]
+    composable_nodes: list[ComposableNode]
+    launch_arguments: list[DeclareLaunchArgument]
+    other_entities: list[LaunchDescriptionEntity]
+
+
+def get_launch_description(package_name: str, launch_file: str) -> LaunchDescription:
     path = Path(get_package_share_directory(package_name)) / "launch" / launch_file
     spec = importlib.util.spec_from_file_location(
         f"{package_name}.launch_file.split('.')[0]", path
@@ -27,34 +33,66 @@ def get_nodes(
     if module is None:
         raise ImportError(f"Could not load module from spec '{spec}'.")
 
-    get_nodes_function_name = "get_nodes"
+    generate_launch_description_function_name = "generate_launch_description"
     error_message = (
-        f"Please define a {get_nodes_function_name} function with the following signature:\n"
-        + f"def {get_nodes_function_name}(**arguments: Any) -> tuple[list[Node], list[ComposableNode]]:\n"
-        + "    # ...\n"
-        + "    # return [...], [...]"
+        f"Please define a {generate_launch_description_function_name} function with the following signature:\n"
+        + f"def {generate_launch_description_function_name}() -> launch.LaunchDescription:"
     )
 
-    if not hasattr(module, get_nodes_function_name):
+    if not hasattr(module, generate_launch_description_function_name):
         raise AttributeError(
-            f"Module {module.__name__} has no {get_nodes_function_name} function.\n"
+            f"Module {module.__name__} has no {generate_launch_description_function_name} function.\n"
             + error_message
         )
 
-    function = getattr(module, get_nodes_function_name)
+    function = getattr(module, generate_launch_description_function_name)
 
     if not inspect.isfunction(function):
         raise AttributeError(
-            f"Attribute {get_nodes_function_name} is not a function.\n" + error_message
+            f"Attribute {generate_launch_description_function_name} is not a function.\n"
+            + error_message
         )
 
     signature = inspect.signature(function)
     parameters = list(signature.parameters.values())
 
-    if len(parameters) != 1:
+    if len(parameters) != 0:
         raise AttributeError(
-            f"Invalid signature for {get_nodes_function_name} function.\n"
+            f"Invalid signature for {generate_launch_description_function_name} function.\n"
             + error_message
         )
 
-    return getattr(module, get_nodes_function_name)(**arguments)
+    return getattr(module, generate_launch_description_function_name)()
+
+
+def extract_launch_description_entities(
+    launch_description: LaunchDescription,
+) -> LaunchDescriptionEntities:
+    nodes: list[Node] = []
+    composable_nodes: list[ComposableNode] = []
+    launch_arguments: list[DeclareLaunchArgument] = []
+    other_entities: list[LaunchDescriptionEntity] = []
+
+    for entity in launch_description.entities:
+        if isinstance(entity, Node):
+            nodes.append(entity)
+        elif isinstance(entity, ComposableNodeContainer):
+            composable_nodes.extend(entity.__composable_node_descriptions)
+        elif isinstance(entity, DeclareLaunchArgument):
+            launch_arguments.append(entity)
+        else:
+            other_entities.append(entity)
+
+    return LaunchDescriptionEntities(
+        nodes,
+        composable_nodes,
+        launch_arguments,
+        other_entities,
+    )
+
+
+def get_launch_description_entities(
+    package_name: str, launch_file: str
+) -> LaunchDescriptionEntities:
+    launch_description = get_launch_description(package_name, launch_file)
+    return extract_launch_description_entities(launch_description)
